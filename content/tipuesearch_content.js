2f2466f89162eb6a662c911cc9d79dc63647c013
41023124 <41023124@nfu.edu.tw>
2023-03-06 10:56:35 +0800

    編輯

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n 123', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n 41023122 \xa0 41023124', 'tags': '', 'url': 'About.html'}]};
